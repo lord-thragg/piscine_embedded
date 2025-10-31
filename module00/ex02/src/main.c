760a306635a8064d539c913d1cf87213693f4070
luluzuri <luluzuri@student.42.fr>
2025-10-31 13:02:56 +0100

    cpp08 finished ?

--- module00/ex02/src/main.c
+++ module00/ex02/src/main.c
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:06:41 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/10/30 17:00:33 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/10/31 10:39:34 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	main(void) {
 	DDRB |= (1 << PB0);
 	DDRD &= ~(1 << PD2);
 
+	uint8_t last_state = 1;
+
 	while (1) {
-		if ( !(PIND & ( 1 << PD2 )) ) {
-			PORTB |= (1 << PB0);
-			PORTB &= ~(1 << PB0);
-		}
+		uint8_t current_state = PIND & (1 << PD2);
+		if (!current_state && last_state)
+			PORTB ^= (1 << PB0);
+		last_state = current_state;
 	}
 
 	return (0);
