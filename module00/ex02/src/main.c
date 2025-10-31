c3037c5d69934e4f243b92e3d8ca0c90b821c9e4
luluzuri <luluzuri@student.42.fr>
2025-11-01 00:27:11 +0100

    Some bug tagged in the code

--- module00/ex02/src/main.c
+++ module00/ex02/src/main.c
@@ -6,9 +6,12 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:06:41 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/10/31 13:03:25 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/11/01 00:26:40 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+// TODO To modify: When pressed too long and relieve the LED turn off/on ( not expected behavior )
 
 #include <util/delay.h>
 #include <avr/io.h>
@@ -21,8 +24,10 @@ int	main(void) {
 
 	while (1) {
 		uint8_t current_state = PIND & (1 << PD2);
-		if (!current_state && last_state)
+		if (!current_state && last_state) {
+			_delay_ms(50); // FIXME Problem here bc the delay is okay but  bad behavior when the button is hold
 			PORTB ^= (1 << PB0);
+		}
 		last_state = current_state;
 	}
 	return (0);
