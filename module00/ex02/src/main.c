c134d9e2fa6c12198d83fd7ae00b24d953128f00
lord-thragg <lord-thragg@student.42.fr>
2025-11-01 14:19:45 +0100

    modification ex02 and creation ex03

--- module00/ex02/src/main.c
+++ module00/ex02/src/main.c
@@ -6,12 +6,9 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:06:41 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/11/01 00:26:40 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/11/01 14:19:38 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
-// TODO To modify: When pressed too long and relieve the LED turn off/on ( not expected behavior )
 
 #include <util/delay.h>
 #include <avr/io.h>
@@ -20,15 +17,15 @@ int	main(void) {
 	DDRB |= (1 << PB0);
 	DDRD &= ~(1 << PD2);
 
-	uint8_t last_state = 1;
+	PORTD |= (1 << PD2);
 
 	while (1) {
-		uint8_t current_state = PIND & (1 << PD2);
-		if (!current_state && last_state) {
-			_delay_ms(50); // FIXME Problem here bc the delay is okay but  bad behavior when the button is hold
-			PORTB ^= (1 << PB0);
-		}
-		last_state = current_state;
+		if ( !(PIND & (1 << PD2)) )
+			PORTB |= (1 << PB0);
+		else
+			PORTB &= ~(1 << PB0);
+		_delay_ms(10);
 	}
+
 	return (0);
 }
--- module00/ex03/src/main.c
+++ module00/ex03/src/main.c
@@ -6,7 +6,7 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:06:41 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/11/01 13:59:27 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/11/01 13:58:11 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,17 @@
 
 int	main(void) {
 	DDRB |= (1 << PB0);
-	PORTB |= (1 << PB0);
+	DDRD &= ~(1 << PD2);
 
+	uint8_t last_state = 1;
+
+	while (1) {
+		uint8_t current_state = PIND & (1 << PD2);
+		if (!current_state && last_state) {
+			_delay_ms(10);
+			PORTB ^= (1 << PB0);
+		}
+		last_state = current_state;
+	}
 	return (0);
 }
