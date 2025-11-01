45f3dde64e645ab28e53e9ebdea40fdc04e55426
lord-thragg <lord-thragg@student.42.fr>
2025-11-02 00:36:44 +0100

    Found the way to turn on the PB4 light ( using mask )

--- module00/ex04/src/main.c
+++ module00/ex04/src/main.c
@@ -6,64 +6,32 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 14:31:17 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/11/01 19:56:20 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/11/02 00:30:22 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <util/delay.h>
-//#include <avr/io.h>
-
+#include <util/delay.h>
+#include <avr/io.h>
 #include <stdio.h>
+#include <stdint.h>
 
 // TODO afficher la valeur en form binaire ( CAD: si la valeur est 5 alors je dois afficher 0b0101 sur les LEDS )
+// TODO Increment with ++ should be the one TO DO need find a way to put that in LEDs
 
 int main( void ) {
-	/*// Pour definir les broches comme etant en sortie
+	// Pour definir les broches comme etant en sortie
 	DDRB |= (1 << PB0);
 	DDRB |= (1 << PB1);
 	DDRB |= (1 << PB2);
 	DDRB |= (1 << PB4);
 	
 	// Value utiliser pour incrementer / decrementer
-	uint8_t value = 0b0000;
-	uint8_t last_state = 1;
+	uint8_t count = 0b0000;
+	uint8_t mask = 0b1000;
 	
-	// Lecture des boutons ( via PINx )
-	while ( 1 ) {
-		uint8_t current_state = PIND & (1 << PD2);
-
-		
+	// operation AND with mask to know if PB3 -> 1
+	int r = (count & mask);
+	if (r) {
+		PORTB |= (1 << PB4);
 	}
-
-	// try to put all the needed LED in light
-	PORTB |= (1 << PB0);
-	PORTB |= (1 << PB1);
-	PORTB |= (1 << PB2);
-	PORTB |= (1 << PB4);*/
-	
-	// essais incrementation de bit
-	unsigned int t = 0b0000;
-	unsigned int mask = 1;
-	
-	// 2
-	t &= ~mask;
-	mask <<= 1;
-	t |= mask;
-	
-	// 4
-	t &= ~mask;
-	mask <<= 1;
-	t |= mask;
-	
-	// 8
-	t &= ~mask;
-	mask <<= 1;
-	t |= mask;
-
-	// 16
-	t &= ~mask;
-	mask <<= 1;
-	t |= mask;
-
-	printf("%u", t);
 }
