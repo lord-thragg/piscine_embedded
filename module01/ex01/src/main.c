d6d1c2857cb97c243cfd3c890705c53a06c16299
lord-thragg <lord-thragg@student.42.fr>
2025-11-06 18:33:36 +0100

    Trying to understand Timer1 things related

--- module01/ex01/src/main.c
+++ module01/ex01/src/main.c
@@ -6,11 +6,12 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 18:06:41 by luluzuri          #+#    #+#             */
-/*   Updated: 2025/11/05 17:07:51 by luluzuri         ###   ########.fr       */
+/*   Updated: 2025/11/06 18:20:43 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 /* ANCHOR Time1
  *	No PORTx register should be used so for that we need use Timer1 register
@@ -22,7 +23,24 @@
 int main( void ) {
 	DDRB |= (1 << PB1); // Permet de mettre la led en sortie
 
+	// Put the mode CTC ( Bit 3 of TCCR1B register )
+	TCCR1B |= (1 << WGM12);
+
+	OCR1A = 7811;
+
+	TIMSK1 |= (1 << OCIE1A); // interupt value
+
+	TCCR1B |= (1 << CS12) | (1 << CS10); // prescaler
+
+	sei();
+
 	while ( 1 ) {
 
 	}
+
+	return (0);
+}
+
+ISR(TIMER1_COMPA_vect) {
+	PORTB ^= (1 << PB1);
 }
